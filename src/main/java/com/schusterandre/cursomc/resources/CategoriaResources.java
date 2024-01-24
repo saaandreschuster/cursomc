2a5f31bc2f5b20ffd8d5823e4c79fb787ed9ea6b
André Schuster <andreartur199@hotmail.com>
2024-01-23 21:42:29 -0400

    Testando a primeira classe de dominio

--- src/main/java/com/schusterandre/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/schusterandre/cursomc/resources/CategoriaResources.java
@@ -1,14 +1,28 @@
 package com.schusterandre.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.schusterandre.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest esta Funcionando!";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+
+		List<Categoria> lista = new ArrayList<>();
+
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 }
