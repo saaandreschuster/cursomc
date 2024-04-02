b25b21ff3c136b9208c227d8daf8659f6ff8b18a
André Schuster <andreartur199@hotmail.com>
2024-04-02 11:56:28 -0400

    inserindo uma categoria

--- src/main/java/com/resources/CategoriaResources.java
+++ src/main/java/com/resources/CategoriaResources.java
@@ -1,11 +1,11 @@
 package com.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.domain.Categoria;
 import com.services.CategoriaService;
@@ -20,5 +20,12 @@ public class CategoriaResources {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/services/CategoriaService.java
+++ src/main/java/com/services/CategoriaService.java
@@ -20,5 +20,9 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), obj));
 	}
-
+	
+	public  Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}	
 }
