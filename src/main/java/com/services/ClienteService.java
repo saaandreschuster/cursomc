0e888631b668e04034892f7dd06f81eb435f16b1
André Schuster <andreartur199@hotmail.com>
2024-04-18 06:09:49 -0400

    ajustes cliente service

--- src/main/java/com/services/ClienteService.java
+++ src/main/java/com/services/ClienteService.java
@@ -1,13 +1,21 @@
 package com.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.domain.Cliente;
 import com.repositories.ClienteRepository;
+import com.services.exceptions.DataIntegrityException;
+
+import dto.ClienteDTO;
 
 @Service
 public class ClienteService {
@@ -21,4 +29,42 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName(), obj));
 	}
 
+	public List<Cliente> findAll() {
+		List<Cliente> list = repo.findAll();
+		return list;
+	}
+
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		repo.findById(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque ha uma entidade relacionadas!");
+		}
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderby, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderby);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
---
+++ src/main/java/dto/ClienteDTO.java
@@ -0,0 +1,57 @@
+package dto;
+
+import java.io.Serializable;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import com.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório do nome.")
+	@Size(min = 5, max=120, message = "O tamanho deve ser entre que 5 e 80 caracteres.")
+	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigatório do E-mail.")
+	@Email(message = "E-mail inválido.")
+	private String email;
+
+	public ClienteDTO() {
+		super();
+	}
+
+	public ClienteDTO(Cliente obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
