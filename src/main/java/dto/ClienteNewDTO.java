41e077120e7640a74666f666e6e1c180b3d4ea7c
André Schuster <andreartur199@hotmail.com>
2024-04-22 20:18:17 -0400

    validacão cpfcnpj

--- src/main/java/dto/ClienteNewDTO.java
+++ src/main/java/dto/ClienteNewDTO.java
@@ -2,36 +2,46 @@ package dto;
 
 import java.io.Serializable;
 
-import org.hibernate.validator.constraints.br.CNPJ;
 import org.hibernate.validator.constraints.br.CPF;
+
+
+import com.services.validation.ClienteInsert;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
 
-	@NotEmpty(message = "Preenchimento obrigatório do nome.")
+	@NotEmpty(message = "Preenchimento obrigatório.")
 	@Size(min = 5, max = 120, message = "O tamanho deve ser entre que 5 e 80 caracteres.")
 	private String nome;
 
-	@NotEmpty(message = "Preenchimento obrigatório do E-mail.")
+	@NotEmpty(message = "Preenchimento obrigatório.")
 	@Email(message = "E-mail inválido.")
 	private String email;
-
+	
+	@NotEmpty(message = "Preenchimento obrigatório.")
 	private String cpfOuCnpj;
 
 	private Integer tipo;
 
+	@NotEmpty(message = "Preenchimento obrigatório.")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório.")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório.")
 	private String cep;
-
+	
+	@NotEmpty(message = "Preenchimento obrigatório.")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
